418f4088c2c1bf32f711ba1ccfe8f854c95a8123
Jason-bolt <appiatujason@gmail.com>
2022-12-16 11:03:12 +0000

    Done with header, working on create task button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CreateButton from './components/CreateButton';
 import Header from './components/Header'
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header />
+      <CreateButton />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 const Header = () => {
   return (
-    <div className="text-3xl">Header</div>
+    <div className="bg-gradient-to-r from-green-500 via-green-700 to-green-500 py-5">
+        <div className="container text-center text-white">
+            <h1 className="text-3xl font-bold">
+                TODO APP
+            </h1>
+        </div>
+    </div>
   )
 }
 
