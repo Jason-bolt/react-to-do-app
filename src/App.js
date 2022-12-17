712c85cd41c0acc7abbc79ab1a85960970cc00e2
Jason-bolt <appiatujason@gmail.com>
2022-12-17 00:59:21 +0000

    Create Task button done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import CreateButton from './components/CreateButton';
 import Header from './components/Header'
 
 
 function App() {
+
+  const [text, setText] = useState("Create Task")
+  const [showForm, setShowForm] = useState(false)
+
+  const toggleForm = () => {
+    setShowForm(!showForm)
+  }
+
+
   return (
     <>
       <Header />
-      <CreateButton />
+      <CreateButton text={text} toggleForm={toggleForm} showForm={showForm} />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="bg-gradient-to-r from-green-500 via-green-700 to-green-500 py-5">
-        <div className="container text-center text-white">
+        <div className="text-center text-white">
             <h1 className="text-3xl font-bold">
                 TODO APP
             </h1>
