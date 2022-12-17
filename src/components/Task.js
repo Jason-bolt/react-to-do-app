eb990de0ef1ad5df2bb41d5e488f22b6ec7f93aa
Jason-bolt <appiatujason@gmail.com>
2022-12-17 03:39:14 +0000

    Added close button to tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,14 @@
+import { IoMdCloseCircle } from "react-icons/io";
+
 const Task = ({ tasks }) => {
   return (
     <>
     {
         tasks.map((task) => (
-        <div className="border p-4 my-2 bg-green-50 w-full md:w-1/2 lg:w-1/3">
+        <div className="border p-4 my-2 bg-green-50 relative w-full md:w-1/2 lg:w-1/3">
+            <div className="absolute top-3 right-3">
+                <IoMdCloseCircle className="text-red-500 text-xl hover:text-2xl" />
+            </div>
             <h1 className="text-lg text-green-900">
                 {task.task}
             </h1>
