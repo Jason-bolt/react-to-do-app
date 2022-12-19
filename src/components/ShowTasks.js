08a722cd9085abfbe3ba9b98a968e6eb1307ce82
Jason-bolt <appiatujason@gmail.com>
2022-12-19 20:27:53 +0000

    Set default value when no tasks are available

--- src/components/ShowTasks.js
+++ src/components/ShowTasks.js
@@ -1,14 +1,16 @@
-import Task from "./Task"
+import Task from "./Task";
 
 const ShowTasks = ({ tasks, onDelete }) => {
   return (
     <section className="my-20 px-4 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-semibold mb-7">
-            My tasks
-        </h1>
+      <h1 className="text-3xl font-semibold mb-7">My tasks</h1>
+      {tasks.length > 0 ? (
         <Task tasks={tasks} onDelete={onDelete} />
+      ) : (
+        "No task created!"
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default ShowTasks
+export default ShowTasks;
